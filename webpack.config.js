03529cd3d1e7b922f655ba7d0f4c69fde6a5b8b4
dsuekamling <dsuekamling@gmail.com>
2023-04-23 11:10:25 -0400

    please work

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // Add this line
 
 module.exports = {
   entry: {
@@ -69,6 +70,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'css/style.[contenthash].css',
     }),
+    new CleanWebpackPlugin(), // Add this line
   ],
   devServer: {
     contentBase: path.join(__dirname, 'public'),
